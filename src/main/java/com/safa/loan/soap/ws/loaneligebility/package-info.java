d78492ad603647ec27c457286df374ef4c6b9971
safa bettieb <safa.bettaieb1@gmail.com>
2020-02-16 13:19:07 +0100

    generate CustomerRequest and WsResponse from xsd file

---
+++ src/main/java/com/safa/loan/soap/ws/loaneligebility/package-info.java
@@ -0,0 +1,9 @@
+//
+// Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.7 
+// Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
+// Généré le : 2020.02.16 à 01:16:12 PM WAT 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.safa.com/loan/soap/ws/loanEligebility", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.safa.loan.soap.ws.loaneligebility;
